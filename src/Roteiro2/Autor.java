c8cc71547b16f2bb8f7f3dc051586c69893ad5d2
FrancineMoraes <francinemoraes08@hotmail.com>
2017-08-13 18:45:32 -0300

    não sei oque colocar aqui - francine
    não sei oque colocar aqui

--- src/Roteiro2/Autor.java
+++ src/Roteiro2/Autor.java
@@ -1,5 +1,50 @@
 package Roteiro2;
 
+import java.time.LocalDate;
+
+/**
+ *
+ * @author User
+ */
 public class Autor {
+    private String autor;
+    private LocalDate nasc;
     
+    public Autor(String autor){
+        this.autor = autor;
+    }
+
+    /**
+     * @return the autor
+     */
+    public String getAutor() {
+        return autor;
+    }
+
+    /**
+     * @param autor the autor to set
+     */
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    /**
+     * @return the nasc
+     */
+    public LocalDate getNasc() {
+        return nasc;
+    }
+
+    /**
+     * @param nasc the nasc to set
+     */
+    public void setNasc(LocalDate nasc) {
+        this.nasc = nasc;
+    }
+    
+    @Override
+    public String toString(){
+        return "Autor: "+this.autor+
+                "Data de nascimento"+this.nasc;
+    }
 }
